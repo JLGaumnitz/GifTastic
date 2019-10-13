9711a3cd2efbb611609ad2ce5251d73a3bbf1355
JLG <mjgaumnitz@kc.rr.com>
2019-10-13 00:07:36 -0500

    updated css, added README, worked on mobile responsiveness

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# GifTastic
-Homework Assignment #6
+GifTastic | Homework Assignment #6
+
+What's Your Reaction!? 
+created by Jennifer Gaumnitz
+
+Here's how the site works:
+
+* This project utilizes the GIPHY API to create a dynamic web page that populates gifs in the category of Reactions (like Wow! Really? and a variety of emotions). 
+
+* When the user clicks on a button, the page retrieves 10 static, non-animated gif images from the GIPHY API and places them on the page.
+
+*  When the user clicks one of the still GIPHY images, the gif animates. If the user clicks the gif again, it returns to the static image.
+
+*  Under every gif, the rating is displayed (PG or G, provided by the GIPHY API).
+
+*  In addition, there is a form that allows a user to input a word to be added to the gifReactions array. Then the user's word is added to the other buttons and will retrieve the appropriate gifs when clicked.
+
+WHY THE PROJECT IS USEFUL
+
+  The GifTastic project fulfills a homework assignment for University of Kansas Coding Boot Camp, August 2019 to February 2020. Besides continuing to work with jQuery and JavaScript, with this assignment, I also began learning how to work with AJAX requests and the GIPHY API to write items dynamically to the DOM without a browser page refresh.
+
+HOW YOU CAN GET STARTED WITH THE PROJECT
+
+  Just push one of the preloaded buttons to retrieve 10 relevant gifs. Create your own button by inputting a word in the field and pushing "Fetch". Then push your new button to retrieve the gifs for your new button. The gifs are cleared with each new button push.
+
+WHERE USERS CAN GET HELP WITH THE PROJECT
+
+  Questions? Contact me at jlgaumnitz@gmail.com
+
+WHO MAINTAINS AND CONTRIBUTES TO THE PROJECT
+
+  Only I do, and I probably won't update it again after completing this assignment. 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,11 @@
-// VARIABLES for Preloaded gifs, number of gifs to retrieve, and rating
+// VARIABLES for array of preloaded buttons, number of gifs to retrieve, and rating
+
+// NOTE to TAs grading this: I labeled my array "gifReactions" instead of "topics" (as the Instructions said).
+
+// Note to self: The unbind() Method is an inbuilt method in jQuery which is used to remove any selected event handlers.
+
 var gifReactions = ["Wow!", "Thank You", "Really?", "Happy", "Angry"];
-var numberOfGIFs = 12;
+var numberOfGIFs = 10;
 var acceptableRating = "PG";
 
 // FUNCTION to render buttons
@@ -19,9 +24,10 @@ function renderButtons(){
 		$("#gif-container").empty();
 		populateGifContainer($(this).text());
 	});
-
 }
-// FUNCTION to add buttons
+
+// FUNCTION to add buttons 
+// (indexOf of -1 means that the input was not already in the array, so the word is pushed to the array)
 function addButton(reaction){
 	if(gifReactions.indexOf(reaction) === -1) {
 		gifReactions.push(reaction);
@@ -30,17 +36,16 @@ function addButton(reaction){
 	}
 }
 
-// Code to retrieve images from Giphy and put them in a new empty div
+// Code to retrieve images from Giphy and display them and their ratings in a new empty div
 function populateGifContainer(reaction){
 	$.ajax({
 		url: "https://api.giphy.com/v1/gifs/search?q=" + reaction + 
 		"&api_key=SEQHdoXfuKPwb6tBQwuuE1c7bj6E53vO&rating=" + acceptableRating + "&limit=" + numberOfGIFs,
-		method: "GET"
+		method: "Get"
 	}).then(function(response){
 		response.data.forEach(function(element){
 			newDiv = $("<div>");
 			newDiv.addClass("individual-gif-container");
-			newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
 			var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
 			newImage.addClass("gif-image");
 			newImage.attr("state", "still");
@@ -48,9 +53,10 @@ function populateGifContainer(reaction){
 			newImage.attr("animated-data", element.images.fixed_height.url);
 			newDiv.append(newImage);
 			$("#gif-container").append(newDiv);
+			newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
 		});
-		// This code animates image or reverts to still image on click
-		$("#gif-container").addClass("ridged-border");
+		// This code animates the gif or reverts to still image on click
+		$("#gif-container").addClass("double-horizontal-rule");
 		$(".gif-image").unbind("click");
 		$(".gif-image").on("click", function(){
 			if($(this).attr("state") === "still") {
